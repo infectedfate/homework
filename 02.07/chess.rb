2bc9ee282fe76d9b1f55e9418ded9b7664470cb3
Malinin Mikhail <infectedfate@gmail.com>
2019-07-03 00:49:11 +0300

    awesome chess!

---
+++ 02.07/chess.rb
@@ -0,0 +1,18 @@
+def letters
+  puts "  ABCDEFGH "
+end
+
+def cells
+  puts row1 = "1 X_X_X_X_ 1"
+  puts row2 = "2 _X_X_X_X 2"
+  puts row3 = "3 X_X_X_X_ 3"
+  puts row4 = "4 _X_X_X_X 4"
+  puts row5 = "5 X_X_X_X_ 5"
+  puts row6 = "6 _X_X_X_X 6"
+  puts row7 = "7 X_X_X_X_ 7"
+  puts row8 = "8 _X_X_X_X 8"
+end
+
+letters
+cells
+letters
